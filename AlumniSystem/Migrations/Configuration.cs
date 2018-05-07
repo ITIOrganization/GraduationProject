29824f6e0dc661f3582747143d271047c1d50e2e
dev-hassan <dev_m_hassan@hotmail.com>
2018-05-07 08:50:01 +0200

    test

--- AlumniSystem/Migrations/Configuration.cs
+++ AlumniSystem/Migrations/Configuration.cs
@@ -5,14 +5,14 @@ namespace AlumniSystem.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<AlumniSystem.Models.ApplicationDbContext>
+    internal sealed class Configuration : DbMigrationsConfiguration<Models.ApplicationDbContext>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
         }
 
-        protected override void Seed(AlumniSystem.Models.ApplicationDbContext context)
+        protected override void Seed(Models.ApplicationDbContext context)
         {
             //  This method will be called after migrating to the latest version.
 
